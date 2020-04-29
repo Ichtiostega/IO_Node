da1091b36acc3a09a2588353451719613e06a20b
Kocierz Karol-JPK738 <karol.kocierz@motorolasolutions.com>
2020-04-29 21:06:04 +0200

    New endpoint

--- test_module.js
+++ test_module.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 2137;
 
+tournaments = ['1 turniej agh', '2 turniej uj', '3 turniej up'];
+
 app.get('/', (req, res) => {
     res.send('Response to base request');
     console.log('Sent response');
+});
+
+app.get('/tournament', (req, res) => {
+    data = '';
+    for(el in tournaments)
+    {
+        data += el + '\n';
+    }
+    res.send(data);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
