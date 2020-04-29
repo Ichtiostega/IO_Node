996bdfdec167a655511cd0799659d48d92f17067
Kocierz Karol-JPK738 <karol.kocierz@motorolasolutions.com>
2020-04-29 20:25:40 +0200

    Added a log

--- test_module.js
+++ test_module.js
@@ -4,6 +4,7 @@ const port = 2137;
 
 app.get('/', (req, res) => {
     res.send('Response to base request');
+    console.log('Sent response');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
